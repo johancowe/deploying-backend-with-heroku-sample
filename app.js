317760f81cca3b3122e50d27926ff539c998579c
Johan Cowe <johancowe@hotmail.com>
2022-08-02 22:05:09 +0200

    welcome

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express(); // create an instance of the express module (app is the c
 const PORT = process.env.PORT || 5000; // use either the host env var port (PORT) provided by Heroku or the local port (5000) on your machine
 
 app.get('/', (req, res) => { // send a get request to root directory ('/' is this file (app.js))
-  res.send('<h1>Welcome to project PANCAKE !!</h1>');
+  res.send('<h1>Welcome to project PANCAKE - from the COWE boyz !!</h1>');
 })
 
 app.listen(PORT, () => { // start server and listen on specified port
